617d17afc58de8a24c74e875c24ed13a4796cb5a
camiloculpian <camiloculpian@gmail.com>
2024-09-22 23:36:57 -0300

    modified:   mi-ciudad/src/app/pages/profile/profile.page.html 	modified:   mi-ciudad/src/app/pages/profile/profile.page.ts

--- mi-ciudad/src/app/pages/profile/profile.page.ts
+++ mi-ciudad/src/app/pages/profile/profile.page.ts
@@ -2,11 +2,9 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonText, IonInput, IonLabel, IonItem, IonAvatar } from '@ionic/angular/standalone';
-import { person, pencil} from 'ionicons/icons';
+import { pencil} from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-import { environment } from 'src/app/app.component';
-import {  Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
 
 @Component({
@@ -32,19 +30,27 @@ export class ProfilePage implements OnInit {
   ngOnInit() {
     
     this.userDataForm  = this.formBuilder.group({
+      username: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(4)]],
       lastName: ['', [Validators.required, Validators.email]],
       name: ['', [Validators.required, Validators.email]],
+      dni: ['', [Validators.required,Validators.minLength(7),Validators.maxLength(8)]],
+      address: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
     });
     const resp = this.authService.getProfile().subscribe({
       next: (resp) => {
         // CORREGIR ACA HAY QUE SETEAR YA SE CREO ARRIBA!!!
         this.userDataForm  = this.formBuilder.group({
+          username: [resp.data.username, [Validators.required]],
           email: [resp.data.email, [Validators.required, Validators.email]],
           password: ['', [Validators.required, Validators.minLength(4)]],
-          lastName: [resp.data.lastName, [Validators.required, Validators.email]],
-          name: [resp.data.name, [Validators.required, Validators.email]],
+          lastName: [resp.data.lastName, [Validators.required]],
+          name: [resp.data.name, [Validators.required]],
+          dni: [resp.data.dni, [Validators.required,Validators.minLength(7),Validators.maxLength(8)]],
+          address: [resp.data.address, [Validators.required]],
+          phone: [resp.data.phone, [Validators.required]],
         });
       },
       error: (error) => {
