26540eb2e473c6245748d411bdda4465f76f3a21
camiloculpian <camiloculpian@gmail.com>
2024-09-24 20:38:16 -0300

    deleted:    backend/dist/app.module.d.ts 	deleted:    backend/dist/app.module.js 	deleted:    backend/dist/app.module.js.map 	modified:   backend/dist/auth/auth.controller.d.ts 	deleted:    backend/dist/auth/auth.module.d.ts 	deleted:    backend/dist/auth/auth.module.js 	deleted:    backend/dist/auth/auth.module.js.map 	deleted:    backend/dist/generated/i18n.generated.d.ts 	deleted:    backend/dist/generated/i18n.generated.js 	deleted:    backend/dist/generated/i18n.generated.js.map 	deleted:    backend/dist/i18n/es-AR/lang.json 	deleted:    backend/dist/main.d.ts 	deleted:    backend/dist/main.js 	deleted:    backend/dist/main.js.map 	deleted:    backend/dist/tsconfig.build.tsbuildinfo 	modified:   backend/src/app.module.ts 	new file:   backend/src/commerce/commerce.controller.spec.ts 	new file:   backend/src/commerce/commerce.controller.ts 	new file:   backend/src/commerce/commerce.module.ts 	new file:   backend/src/commerce/commerce.service.spec.ts 	new file:   backend/src/commerce/commerce.service.ts 	new file:   backend/src/commerce/dto/create-commerce.dto.ts 	new file:   backend/src/commerce/dto/update-commerce.dto.ts 	new file:   backend/src/commerce/entities/commerce.entity.ts

---
+++ backend/src/commerce/dto/update-commerce.dto.ts
@@ -0,0 +1,34 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCommerceDto } from './create-commerce.dto';
+
+// export class UpdateCommerceDto extends PartialType(CreateCommerceDto) {}
+
+
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { Horario, Tramite } from '../entities/commerce.entity';
+
+export class UpdateCommerceDto {
+    @IsOptional()
+    @IsString()
+    nombre?: string;
+
+    @IsOptional()
+    @IsString()
+    descripcion?: string;
+
+    @IsOptional()
+    @IsString()
+    ubicacion?: string;
+
+    @IsOptional()
+    @IsEnum(Tramite)
+    horario?: Horario;
+
+    @IsOptional()
+    @IsString()
+    imagen?: string;
+
+    @IsOptional()
+    @IsEnum(Tramite)
+    tramite?: Tramite; 
+}
