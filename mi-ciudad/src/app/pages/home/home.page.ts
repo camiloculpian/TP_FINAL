fb8a4b7e99c5bd690e431682f2d308b75698d8d1
camiloculpian <camiloculpian@gmail.com>
2024-10-19 08:53:08 -0300

    modified:   mi-ciudad/android/app/src/main/AndroidManifest.xml.orig 	modified:   mi-ciudad/src/app/pages/home/home.page.html 	modified:   mi-ciudad/src/app/pages/home/home.page.ts

--- mi-ciudad/src/app/pages/home/home.page.ts
+++ mi-ciudad/src/app/pages/home/home.page.ts
@@ -1,21 +1,51 @@
-import { Component, Output, output } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IonInput, IonText, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonIcon } from '@ionic/angular/standalone';
+import { IonInput, IonText, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonIcon, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { environment } from 'src/app/app.component';
 import { person} from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonTabBar, IonTabs, IonRouterOutlet, IonApp, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonText],
+  imports: [IonLabel, IonItem, IonIcon, IonTabBar, IonTabs, IonRouterOutlet, IonApp, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonText, FormsModule, ReactiveFormsModule, NgIf],
 })
 export class HomePage {
-  constructor(private router: Router) {
+  mostrarFormulario = false;
+  public localComercialDataForm!: FormGroup;
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router
+  ) {
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.localComercialDataForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      nombre_negocio: ['', [Validators.required]],
+      correo: ['', [Validators.required]],
+      telefono: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      foto: ['', [Validators.required]]
+    })
   }
   navigateToProfile(){
     this.router.navigate(['profile']);
+  }
+
+  enviarFormulario() {
+    console.log('Datos del formulario:', this.localComercialDataForm);
+    this.limpiarFormulario();
+    this.mostrarFormulario = false; 
+  }
+
+  limpiarFormulario() {
+    this.localComercialDataForm.reset()
   }
 }
