3bab4d3f0bf6da45955035a3b48464117e5c27ae
camiloculpian <camiloculpian@gmail.com>
2024-10-19 14:06:24 -0300

    modified:   backend/dist/tsconfig.build.tsbuildinfo 	new file:   mi-ciudad/android/app/src/main/AndroidManifest.xml.orig 	modified:   mi-ciudad/src/app/app.component.ts

--- mi-ciudad/src/app/app.component.ts
+++ mi-ciudad/src/app/app.component.ts
@@ -40,9 +40,7 @@ export class AppComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.platform.ready().then(() => {
-      this.verificarConexion()
-    })
+    this.verificarConexion()
   }
 
   async verificarConexion() {
