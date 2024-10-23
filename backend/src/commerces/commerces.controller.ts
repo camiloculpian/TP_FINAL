73d6800152ad33c9ccf8c99b2c9bb77c98ff3019
camiloculpian <camiloculpian@gmail.com>
2024-10-22 23:32:49 -0300

    modified:   backend/dist/tsconfig.build.tsbuildinfo 	modified:   backend/src/commerces/commerces.controller.ts 	modified:   backend/src/commerces/commerces.service.ts

--- backend/src/commerces/commerces.controller.ts
+++ backend/src/commerces/commerces.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CommercesService } from './commerces.service';
+import { CreateCommerceDto } from './dto/create-commerce.dto';
+import { UpdateCommerceDto } from './dto/update-commerce.dto';
 
-@Controller('commerces')
-export class CommercesController {}
+@Controller('commerce')
+export class CommercesController {
+  constructor(private readonly commerceService: CommercesService) {}
+
+  @Post()
+  create(@Body() createCommerceDto: CreateCommerceDto) {
+    return this.commerceService.create(createCommerceDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.commerceService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.commerceService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCommerceDto: UpdateCommerceDto) {
+    return this.commerceService.update(+id, updateCommerceDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.commerceService.remove(+id);
+  }
+}
+
