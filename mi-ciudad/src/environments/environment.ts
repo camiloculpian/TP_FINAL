1d6e149396e5cbb8ba525ae9356afd1bdf018658
Natasha Szmagalski <133432196+Raven-Szmagalski@users.noreply.github.com>
2024-11-20 20:39:05 -0300

    modified:   mi-ciudad/src/app/layouts/main-content/main-content.component.html 	modified:   mi-ciudad/src/app/pages/commerce/commerce.page.html 	modified:   mi-ciudad/src/app/pages/home/home.page.html 	modified:   mi-ciudad/src/environments/environment.prod.ts 	modified:   mi-ciudad/src/environments/environment.ts

--- mi-ciudad/src/environments/environment.ts
+++ mi-ciudad/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // de Camilo
-  apiURL: 'http://10.68.1.100:3000',
+  apiURL: 'http://192.168.100.4:3000',
   apiVersion: '/api/v1',
   loggedIn: false,
   username: '',
