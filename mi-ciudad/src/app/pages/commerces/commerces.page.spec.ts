9d814f66f76754b2a4f02565245a9aaa6a232af4
camiloculpian <camiloculpian@gmail.com>
2024-11-26 16:40:49 -0300

    modified:   mi-ciudad/src/app/pages/commerces/commerces.page.spec.ts

--- mi-ciudad/src/app/pages/commerces/commerces.page.spec.ts
+++ mi-ciudad/src/app/pages/commerces/commerces.page.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './commerces.page';
+import { CommercesPage } from './commerces.page';
 
 describe('HomePage', () => {
   let component: CommercesPage;
-  let fixture: ComponentFixture<HomePage>;
+  let fixture: ComponentFixture<CommercesPage>;
 
   beforeEach(async () => {
     fixture = TestBed.createComponent(CommercesPage);
